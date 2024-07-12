18bb64d0ff83ca70119ecf32d973a83dbebe0ed5
MusaAjmal <musaajmal56@gmail.com>
2024-07-13 00:47:28 +0500

    added Scriptable Objects

---
+++ Assets/Scripts/EquipableObjects.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipableObjects : MonoBehaviour
+{
+    [SerializeField] private InventoryItemsSO itemSO;
+}
