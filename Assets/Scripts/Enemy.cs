63fb82a67ce3c8881eb458b07c127a0da517e1bc
MusaAjmal <musaajmal56@gmail.com>
2024-07-12 15:14:52 +0500

    changed stuff

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -11,17 +11,44 @@ public class Enemy : MonoBehaviour
 {
 
    [SerializeField] private GameObject[] movePoints;
+   [SerializeField] private float moveSpeed;
+    private Vector3 faceDirection;
     private int targetPoint;
-    [SerializeField] private float moveSpeed;
     private float constantY = 1.07f;
-    private GameObject temp;
     private bool isIncreasing;
+    private float rotateSpeed = 5f;
+    private float lookDistance = 100f;
+    private Vector3 lastInteractDirection;
+    private enum EnemyState
+    {
+        Patrolling,
+        Alerted
+    }
+    private EnemyState currentState;
+
     private void Start()
     {
+        currentState = EnemyState.Patrolling;
         targetPoint = 0;    
     }
 
     private void Update()
+    {
+        ChasePlayer();
+        switch (currentState) {
+
+            case EnemyState.Patrolling:
+                Patrol();
+                break;
+
+            case EnemyState.Alerted:
+
+                break;
+
+        }
+           
+    }
+    private void Patrol()
     {
         Vector3 targetPosition = new Vector3(movePoints[targetPoint].transform.position.x, constantY, movePoints[targetPoint].transform.position.z);
         Vector3 currentPosition = new Vector3(transform.position.x, constantY, transform.position.z);
@@ -39,6 +66,24 @@ public class Enemy : MonoBehaviour
         }
 
         transform.position = Vector3.MoveTowards(currentPosition, targetPosition, moveSpeed * Time.deltaTime);
+        faceDirection = (targetPosition - transform.position).normalized;
+
+        if (isMoving())
+        {
+            lastInteractDirection = transform.position;
+            transform.forward = Vector3.Slerp(transform.forward, faceDirection, rotateSpeed * Time.deltaTime);
+        }
+
+    }
+    
+    private void ChasePlayer()
+    {
+        if (Physics.Raycast(transform.position,transform.forward,out RaycastHit raycastHit,lookDistance))
+        {
+            if (raycastHit.transform.TryGetComponent(out Player player)) {
+                Debug.Log("Chasing");
+            }
+        }
     }
 
     private void IncreaseTarget()
@@ -60,5 +105,8 @@ public class Enemy : MonoBehaviour
             isIncreasing = true;
         }
     }
-
+    private bool isMoving()
+    {
+        return faceDirection != Vector3.zero;
+    }
 }
