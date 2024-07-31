10163f5a588e7e4dae62e07a4cec801bf49eb26a
Ahmed-Mansur <FA22-BCS-183@cuilahore.edu.pk>
2024-07-31 12:33:46 -0700

    fixed stone stuff -_-

--- Assets/Scripts/Stone.cs
+++ Assets/Scripts/Stone.cs
@@ -1,41 +1,46 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Stone : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Collision Happened!" + gameObject.tag);
-        Debug.Log(collision.gameObject.layer);
-
-        if (collision.gameObject.CompareTag("Stone"))
+        // Ensure the collision is only processed if this stone is the one involved in the collision
+        if (collision.gameObject == gameObject)
         {
-            Debug.Log("Works");
-            Vector3 noisePosition = collision.contacts[0].point;
-            Enemy[] enemyControllers = FindObjectsOfType<Enemy>();
-            BigEnemy[] bigEnemies = FindObjectsOfType<BigEnemy>();
+            Debug.Log("Collision Happened!" + gameObject.tag);
+            Debug.Log(collision.gameObject.layer);
 
-            // Combine both types of enemies into a single collection
-            System.Collections.Generic.List<MonoBehaviour> allEnemies = new System.Collections.Generic.List<MonoBehaviour>();
-            allEnemies.AddRange(enemyControllers);
-            allEnemies.AddRange(bigEnemies);
-
-            // Find the nearest enemy
-            MonoBehaviour nearestEnemy = FindNearestEnemy(allEnemies, noisePosition);
-            if (nearestEnemy is Enemy)
+            if (collision.gameObject.CompareTag("Stone"))
             {
-                (nearestEnemy as Enemy).CheckDistraction(noisePosition, gameObject);
-            }
-            else if (nearestEnemy is BigEnemy)
-            {
-                (nearestEnemy as BigEnemy).CheckDistraction(noisePosition, gameObject);
-            }
+                Debug.Log("Works");
+                Vector3 noisePosition = collision.contacts[0].point;
+                Enemy[] enemyControllers = FindObjectsOfType<Enemy>();
+                BigEnemy[] bigEnemies = FindObjectsOfType<BigEnemy>();
 
-            // Destroy the stone object after the collision
-            Destroy(collision.gameObject);
+                // Combine both types of enemies into a single collection
+                List<MonoBehaviour> allEnemies = new List<MonoBehaviour>();
+                allEnemies.AddRange(enemyControllers);
+                allEnemies.AddRange(bigEnemies);
+
+                // Find the nearest enemy
+                MonoBehaviour nearestEnemy = FindNearestEnemy(allEnemies, noisePosition);
+                if (nearestEnemy is Enemy)
+                {
+                    (nearestEnemy as Enemy).CheckDistraction(noisePosition, gameObject);
+                }
+                else if (nearestEnemy is BigEnemy)
+                {
+                    (nearestEnemy as BigEnemy).CheckDistraction(noisePosition, gameObject);
+                }
+
+                // Destroy the stone object after the collision
+                Destroy(gameObject);
+            }
         }
     }
 
-    private MonoBehaviour FindNearestEnemy(System.Collections.Generic.List<MonoBehaviour> enemies, Vector3 position)
+    private MonoBehaviour FindNearestEnemy(List<MonoBehaviour> enemies, Vector3 position)
     {
         MonoBehaviour nearestEnemy = null;
         float smallestDistance = float.MaxValue;
