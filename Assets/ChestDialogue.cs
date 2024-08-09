c5f3d483d096ec39245a0d3a01750df7c02decf0
MusaAjmal <musaajmal56@gmail.com>
2024-08-09 08:19:51 +0500

    kids

--- Assets/ChestDialogue.cs
+++ Assets/ChestDialogue.cs
@@ -4,16 +4,35 @@ using UnityEngine;
 
 public class ChestDialogue : MonoBehaviour
 {
+    [SerializeField] public GameObject obj;
+    private void Start()
+    {
+       // obj = GameObject.FindWithTag("Dialogue");
+    }
     public void appear()
     {
-        gameObject.SetActive(true);
-        transform.LeanMoveLocal(new Vector2(57, 479), 1).setEaseOutQuart();
-        StartCoroutine(DisableAfterDelay(1.5f));
+        if (obj != null) {
+            obj.SetActive(true);
+            obj.transform.LeanMoveLocal(new Vector2(57, 479), 1).setEaseOutQuart();
+            StartCoroutine(DisableAfterDelay(1.5f));
+        }
+        else
+        {
+            obj = GameObject.FindWithTag("Dialogue");
+        }
+        
     }
 
     private IEnumerator DisableAfterDelay(float delay)
     {
-        yield return new WaitForSeconds(delay);
-        gameObject.SetActive(false);
+        if (obj != null) {
+            yield return new WaitForSeconds(delay);
+            obj.SetActive(false);
+        }
+        else
+        {
+            obj = GameObject.FindWithTag("Dialogue");
+        }
+
     }
 }
